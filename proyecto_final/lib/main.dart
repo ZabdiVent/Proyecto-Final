a44ce690e7513ba81eef74b6d2c99f5568dd9df8
castroallan45 <castroallan45@gmail.com>
2024-12-03 00:54:55 +0000

    4) creacion de main

--- proyecto_final/lib/main.dart
+++ proyecto_final/lib/main.dart
@@ -1,1 +1,22 @@
-//comentario para validacion de pushw
+import 'package:flutter/material.dart';
+import 'home.dart';  
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Tienda de ropa online',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),  // Llamamos al widget de MyHomePage desde home.dart
+    );
+  }
+}
